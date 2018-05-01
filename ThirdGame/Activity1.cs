e31f9cd4c9cf703145eb94fcce5c0856aaa356aa
Ricardo Valente <valentemesmo@gmail.com>
2018-05-01 10:57:22 -0300

    Implementing wifi direct connection

--- ThirdGame/Activity1.cs
+++ ThirdGame/Activity1.cs
@@ -10,25 +10,18 @@ using System.Collections.Generic;
 
 namespace ThirdGame
 {
-    public interface WifiDirect
-    {
-        Func<IEnumerable<WifiP2pDevice>> OnPeersChanged { get; set; }
-        void Connect(WifiP2pDevice device);
-    }
 
-    public class AndroidWifiDirect2 : WifiDirect
-    {
-        public Func<IEnumerable<WifiP2pDevice>> OnPeersChanged { get ; set; }
-
-        public void Connect(WifiP2pDevice device)
-        {
-            
-        }
-    }
 
     public class MyActionListener : WifiP2pManager.IActionListener
     {
+        private readonly Action OnSuccessHandler;
+
         public IntPtr Handle { get; set; }
+
+        public MyActionListener(Action OnSuccessHandler)
+        {
+            this.OnSuccessHandler = OnSuccessHandler;
+        }
 
         public void Dispose()
         {
@@ -36,10 +29,12 @@ namespace ThirdGame
 
         public void OnFailure([GeneratedEnum] WifiP2pFailureReason reason)
         {
+
         }
 
         public void OnSuccess()
         {
+            OnSuccessHandler();
         }
     }
 
@@ -62,18 +57,22 @@ namespace ThirdGame
 
 
             var mManager = (WifiP2pManager)GetSystemService(WifiP2pService);
-            var mChannel = mManager.Initialize(this, MainLooper, null);
+            var mChannel = mManager.Initialize(this, MainLooper, null);//mainloop mesmo!?
 
-            mManager.DiscoverPeers(mChannel, new MyActionListener());
+            mManager.DiscoverPeers(mChannel, new MyActionListener(() =>
+            {
 
-            mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+            }));
+
+            AndroidWifiDirect2 aa = new AndroidWifiDirect2(mManager, mChannel);
+            mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, aa, this);
 
             mIntentFilter = new IntentFilter();
             mIntentFilter.AddAction(WifiP2pManager.WifiP2pStateChangedAction);
             mIntentFilter.AddAction(WifiP2pManager.WifiP2pPeersChangedAction);
             mIntentFilter.AddAction(WifiP2pManager.WifiP2pConnectionChangedAction);
             mIntentFilter.AddAction(WifiP2pManager.WifiP2pThisDeviceChangedAction);
- RegisterReceiver(mReceiver, mIntentFilter);
+            RegisterReceiver(mReceiver, mIntentFilter);
 
             var g = new Game1();
             SetContentView((View)g.Services.GetService(typeof(View)));
@@ -83,8 +82,8 @@ namespace ThirdGame
         protected override void OnResume()
         {
             base.OnResume();
-           
-        }        
+
+        }
 
         protected override void OnPause()
         {
